9c6c762928bda587e4063bedfcdb0dcf169759e7
KDJ <ehdwn7531@naver.com>
2023-11-26 04:45:25 +0900

    - 절대 강좌 유니티 2021
    - 1. 유니티 엔진의 소개
    - 2. 게임 개발 준비
    - 3. 게임 스테이지 제작

---
+++ SpaceShooter/Library/PackageCache/com.unity.ide.rider@3.0.26/CHANGELOG.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01f604b976563f5a24f37bdbc3a19c1d7b9876db34e667f7c34a96472450b56e
+size 10451
---
+++ SpaceShooter/Library/PackageCache/com.unity.ide.rider@3.0.26/Rider/Editor/RiderScriptEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:abca736f9cefff83754f5e089ede41ead9dbaa0946ed8c5d5eca8b1b54ee01de
+size 16065
