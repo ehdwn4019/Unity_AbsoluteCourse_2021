fb8955d26142d16451557f751a9e4fa08f16260b
KDJ <ehdwn7531@naver.com>
2023-11-27 02:41:52 +0900

    - 주인공 캐릭터 제작

---
+++ SpaceShooter/Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0929aaf5266dd02a486458d55153ca8787d9954aca0f3dce79cafc3e72edb5b7
+size 1215
---
+++ SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f2b270d49f6c4bc996c6b5c1567e373d830b696ee35f5e4cf2208de73f83b5e
+size 1517
