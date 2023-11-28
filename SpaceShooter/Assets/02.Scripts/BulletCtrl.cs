36d5abd01e1fac9d16f417bffa772bc5f5d44d7f
KDJ <ehdwn7531@naver.com>
2023-11-29 02:17:55 +0900

    - 5. 총 발사 로직
    - 총알 모델 준비
    - RigidBody 컴포넌트
    - 물리 엔진 속성 설정 - PhysicsManager
    - Collider 컴포넌트
    - 충돌 감지 조건
    - 충돌 이벤트
    - Tag 활용
    - 총알 발사 로직
    -

---
+++ SpaceShooter/Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6b982c03a4a660f9dd6f034b2777b9624e92bcbdfe61e7163993d66c77d6040
+size 445
---
+++ SpaceShooter/Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ecd88d37e6672071bb2a02fea354d673d3e736a26a7ebf924d6445ee4e14b87
+size 322
