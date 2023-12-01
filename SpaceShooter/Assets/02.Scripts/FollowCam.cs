0ed1c3861d3f86a0a729c1315d758572505125a6
KDJ <ehdwn7531@naver.com>
2023-12-02 01:06:15 +0900

    - 한글 인코딩 깨지는 이슈 수정 및 gitIgnore 추가

--- SpaceShooter/Assets/02.Scripts/FollowCam.cs
+++ SpaceShooter/Assets/02.Scripts/FollowCam.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0929aaf5266dd02a486458d55153ca8787d9954aca0f3dce79cafc3e72edb5b7
-size 1215
+oid sha256:c45737a9563b25309cab1a2ed59c9aa9d7df8fc0acba49c1a67e11eaa4ba43b1
+size 1227
--- SpaceShooter/Library/PackageCache/@8c41bf957e/README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fd809731d0b1165f12f053e64bdb8c1d0f4108ed7886f6390326b5742104898f
-size 2771
