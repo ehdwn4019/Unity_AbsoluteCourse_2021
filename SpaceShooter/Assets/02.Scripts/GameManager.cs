62e09fa9b87cdda9bc216a996bd9f1c4788e67b5
KDJ <ehdwn7531@naver.com>
2024-01-25 01:14:14 +0900

    8. 게임 매니저
    - 적 캐릭터의 출현 로직

---
+++ SpaceShooter/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Transform> points = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
+
+        //spawnPointGroup?.GetComponentsInChildren<Transform>(points);
+
+        foreach(Transform point in spawnPointGroup)
+        {
+            points.Add(point);
+        }
+    }
+}
