23cfc06e70195abd213538840bc8ca051ec2863a
KDJ <ehdwn7531@naver.com>
2024-02-13 02:38:51 +0900

    10. 내비게이션 고급 기법
    - 동적 장애물
    - NavMeshObstacle 컴포넌트
    - Off Mesh Link Generation
    - 사용자 정의 Off Mesh Link
    - 자연스러운 회전 처리 ( 네비게이션 회전 처리가 아닌 별도 회전처리 )
    - Area Mask의 활용
    - 경로의 가중치

--- SpaceShooter/Assets/02.Scripts/MonsterCtrl.cs
+++ SpaceShooter/Assets/02.Scripts/MonsterCtrl.cs
@@ -61,11 +61,23 @@ public class MonsterCtrl : MonoBehaviour
 
         agent = GetComponent<NavMeshAgent>();
 
+        agent.updateRotation = false;
+
         //agent.destination = playerTr.position;
 
         anim = GetComponent<Animator>();
 
         bloodEffect = Resources.Load<GameObject>("BloodSprayEffect");
+    }
+
+    private void Update()
+    {
+        if(agent.remainingDistance >= 2.0f)
+        {
+            Vector3 direction = agent.desiredVelocity;
+            Quaternion rot = Quaternion.LookRotation(direction);
+            monsterTr.rotation = Quaternion.Slerp(monsterTr.rotation, rot, Time.deltaTime * 10.0f);
+        }
     }
 
     IEnumerator CheckMonsterState()
